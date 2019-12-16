869ea53e868685d82c64eeb589a9acab95b628a9
sherif20766 <sherif20766@hotmail.com>
2019-12-16 17:06:08 +0000

    created Action creator and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const songsReducer = () => {
+	return [
+		{ title: "No Scrubs", duration: "4:05" },
+		{ title: "Macarena", duration: "2:30" },
+		{ title: "All Star", duration: "3:15" },
+		{ title: "I want it that way", duration: "3:00" }
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
+};
