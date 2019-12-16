d69141240e05c3930fd5b3be435cf5792bbf5c1c
sherif20766 <sherif20766@hotmail.com>
2019-12-16 17:27:25 +0000

    fixed errors in reducers file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { combinedReducers } from "redux";
+import { combineReducers } from "redux";
 
 const songsReducer = () => {
 	return [
@@ -13,9 +13,11 @@ const selectedSongReducer = (selectedSong = null, action) => {
 	if (action.type === "SONG_SELECTED") {
 		return action.payload;
 	}
+
+	return selectedSong;
 };
 
-export default combinedReducers({
+export default combineReducers({
 	songs: songsReducer,
 	selectedSong: selectedSongReducer
 });
